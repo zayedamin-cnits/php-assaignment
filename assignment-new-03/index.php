041e9fea61273c1e6db90265bd79dce63e70e026
zayedamin-cnits <zayedrohan.cnits@gmail.com>
2024-05-19 02:50:49 +0600

    5th commit

---
+++ assignment-new-03/index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";
+$sum = 0;
+
+for ($i = 0; $i <= 30; $i++) {
+
+    echo $i;
+
+    $sum += $i;
+   
+}
+echo "<br>";
+echo  "total namber is ".$sum;
+
+?>
