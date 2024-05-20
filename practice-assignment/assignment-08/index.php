dee183ff6f3e6c3ce71c5512c90861427b549289
zayedamin-cnits <zayedrohan.cnits@gmail.com>
2024-05-20 17:12:35 +0600

    6th commit

---
+++ practice-assignment/assignment-08/index.php
@@ -0,0 +1,23 @@
+<?php
+
+// for($str = 1 ; $str <= 10; $str++){
+//     if($str < 10){
+//         echo  $str ."-";
+//     }else{
+//         echo  $str;
+//     }
+   
+// }
+
+for($sum = 0; $sum<=30; $sum++){;
+   $x = 1 ;
+   $sum +=$x;
+
+echo $sum;
+}
+
+
+
+
+
+?>
